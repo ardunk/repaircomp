ba36010fee1b6f19c3d60af68a77831fec4772bd
ardunk <ardunk038@gmail.com>
2021-02-11 16:26:22 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+  //menu navbar
+  $('#header-layout-button').click(function() {
+    $('#header-layout-navbar').toggleClass("open");
+    $(this).toggleClass("open");
+  });
+  //next function
+});
